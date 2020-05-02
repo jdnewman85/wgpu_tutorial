98090cdc19d80dbbdebba32f2100f11efa0e11df
James Newman <jdnewman85@gmail.com>
2020-05-02 17:50:18 -0600

    Draw triangle with shaders

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,56 @@ fn main() {
     };
     let mut swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
+    // Load shaders
+    let vs_src = include_str!("../shaders/triangle.vert");
+    let vs_spirv = glsl_to_spirv::compile(vs_src, glsl_to_spirv::ShaderType::Vertex).unwrap();
+    let vs_data = wgpu::read_spirv(vs_spirv).unwrap();
+    let vs_module = device.create_shader_module(&vs_data);
+
+    let fs_src = include_str!("../shaders/triangle.frag");
+    let fs_spirv = glsl_to_spirv::compile(fs_src, glsl_to_spirv::ShaderType::Fragment).unwrap();
+    let fs_data = wgpu::read_spirv(fs_spirv).unwrap();
+    let fs_module = device.create_shader_module(&fs_data);
+
+    let render_pipeline_layout = device.create_pipeline_layout(
+        &wgpu::PipelineLayoutDescriptor{bind_group_layouts: &[]}
+    );
+    let render_pipeline = device.create_render_pipeline(
+        &wgpu::RenderPipelineDescriptor{
+            layout: &render_pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor{
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor{
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor{
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            color_states: &[
+                wgpu::ColorStateDescriptor{
+                    format: sc_desc.format,
+                    color_blend: wgpu::BlendDescriptor::REPLACE,
+                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                },
+            ],
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            depth_stencil_state: None,
+            vertex_state: wgpu::VertexStateDescriptor{
+                index_format: wgpu::IndexFormat::Uint16,
+                vertex_buffers: &[],
+            },
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+                });
 
     event_loop.run(move |event, _src_window, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -79,31 +129,34 @@ fn main() {
                         label: Some("Render Encoder"),
                     }
                 );
-                {
-                let _render_pass = encoder.begin_render_pass(
-                    &wgpu::RenderPassDescriptor{
-                        color_attachments: &[
-                            wgpu::RenderPassColorAttachmentDescriptor{
-                                attachment: &frame.view,
-                                resolve_target: None,
-                                load_op: wgpu::LoadOp::Clear,
-                                store_op: wgpu::StoreOp::Store,
-                                clear_color: wgpu::Color {
-                                    r: 0.0,
-                                    g: 0.0,
-                                    b: 0.7,
-                                    a: 1.0,
-                                },
-                            }
-                        ],
-                        depth_stencil_attachment: None,
-                    }
-                );
-                }
 
-                queue.submit(&[
-                             encoder.finish(),
-                ]);
+                {
+                    let mut render_pass = encoder.begin_render_pass(
+                        &wgpu::RenderPassDescriptor{
+                            color_attachments: &[
+                                wgpu::RenderPassColorAttachmentDescriptor{
+                                    attachment: &frame.view,
+                                    resolve_target: None,
+                                    load_op: wgpu::LoadOp::Clear,
+                                    store_op: wgpu::StoreOp::Store,
+                                    clear_color: wgpu::Color {
+                                        r: 0.0,
+                                        g: 0.0,
+                                        b: 0.7,
+                                        a: 1.0,
+                                    },
+                                }
+                            ],
+                            depth_stencil_attachment: None,
+                        }
+                    );
+
+                    render_pass.set_pipeline(&render_pipeline);
+                    render_pass.draw(0..3, 0..1);
+                } //Stop borrowing encoder
+
+
+                queue.submit(&[ encoder.finish(), ]);
             },
             Event::MainEventsCleared => { window.request_redraw(); },
             _ => {}
